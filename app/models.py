724972a2cc1c64de193cd9809577542155b566ee
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-06 10:54:18 +1200

    Added basic routes and setup models for database. Also added basic user login system and teams page. Added watchlist routs and models with basic setup in teams page

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    watchlist = db.relationship('Watchlist', backref='user', uselist=False)
+
+class Team(db.Model):
+    __tablename__ = 'Team'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    formed = db.Column(db.String(100),  nullable=False)
+    size = db.Column(db.Integer)
+
+class Competition(db.Model):
+    __tablename__ = 'Competition'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    format = db.Column(db.String(100), nullable=False)
+    prize = db.Column(db.String(100), nullable=False)
+
+class Ground(db.Model):
+    __tablename__ = 'Ground'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+class Team_Ground(db.Model):
+    __tablename__ = 'Team_Ground'
+    tid = db.Column(db.Integer, db.ForeignKey('team.id'), primary_key=True)
+    pid = db.Column(db.Integer, db.ForeignKey('ground.id'), primary_key=True)
+
+class Watchlist(db.Model):
+    __tablename__ = 'watchlist'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    items = db.relationship('WatchlistItem', backref='watchlist', lazy=True)
+
+class WatchlistItem(db.Model):
+    __tablename__ = 'watchlist_items'
+    id = db.Column(db.Integer, primary_key=True)
+    watchlist_id = db.Column(db.Integer, db.ForeignKey('watchlist.id'), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    team = db.relationship('Team', backref='watchlist_items')
---
+++ app/routes.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user, logout_user, current_user, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app, db
+from app.models import User, Ground, Competition, Team, Team_Ground, WatchlistItem, Watchlist
+
+#  instalise Flask Login 
+login_manager = LoginManager(app)
+login_manager.login.view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/teams', meothods=['GET'])
+def menu():
+    teams = Team.query.all
+    return render_template('/teams.html', teams = teams)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Check your username and password.', 'error')
+    return render_template('login.html')
+
+
+@app.route('/add_to_watchlist', methods=['POST'])
+@login_required
+def add_to_watchlist():
+    team_id = request.form.get('pizza_id')
+
+    team = Team.query.get(team_id)
+    
+    if not team:
+        flash('Invalid team selection', 'error')
+        return redirect(url_for('menu'))
+    
+    watchlist = current_user.watchlist
+    if not watchlist:
+        watchlist = watchlist(user_id=current_user.id)
+        db.session.add(watchlist)
+    
+    watchlist_item = WatchlistItem.query.filter_by(watchlist_id=watchlist.id, team_id=team_id).first()
+    
+    if watchlist_item:
+        watchlist_item.quantity += quantity
+    else:
+        watchlist_item = WatchlistItem(watchlist_id=watchlist.id, taem_id=team_id)
+        db.session.add(cart_item)
+    
+    db.session.commit()
+    flash('Team added to Watchlist successfully!', 'success')
+    return redirect(url_for('menu'))
+
+
+
+@app.route('/view_watchlist')
+@login_required
+def view_watchlist():
+    watchlist = current_user.watchlist
+    watchlist_items = WatchlistItem.query.filter_by(watchlist_id=watchlist.id).all() if watchlist else []
+    return render_template('watchlist.html', cart_items=watchlist_items)
+
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
